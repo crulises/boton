412adfe553a73243ee91baef7897da1b8e513f29
Joaquin Olmos <joaquin.olmos@datalytics.com>
2025-10-07 22:25:17 +0000

    ahi tenes json del mal

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 export LLM_PROVIDER=azure_openai
-export LLM_ENDPOINT=https://api.openai.com/../bla..bla..bla
+export LLM_ENDPOINT=bla-bla
 export LLM_API_KEY=un_super_secreto
 export GITHUB_TOKEN=otro_super_secreto
-export PROMPT_FILE=config/prompts.yaml
+# correr desde app | si no poner prompts y correr desde tests
+export PROMPT_FILE=boton/tests/prompts.yml
+# correr dentro del contenedor
+export GITHUB_REPOSITORY=pruebas
